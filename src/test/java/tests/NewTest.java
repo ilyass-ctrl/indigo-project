dc9952dc836a51f913122ccb02c21f45bf597070
ilyass-ctrl <ilyass.saighi@gmail.com>
2019-10-24 11:11:10 -0400

    first commit on loginBranch

--- src/test/java/tests/NewTest.java
+++ src/test/java/tests/NewTest.java
@@ -5,15 +5,17 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 
 public class NewTest {
-  @Test
-  public void f() {
-  }
+  
   @BeforeTest
   public void beforeTest() {
   }
 
   @AfterTest
   public void afterTest() {
+  }
+  
+  @Test
+  public void f() {
   }
 
 }
